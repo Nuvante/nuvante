bea760e897dc90d416d1d737e6b01a6b88b947a2
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2025-01-03 01:57:47 +0530

    route.ts updated

--- app/api/populate/route.ts
+++ app/api/populate/route.ts
@@ -19,45 +19,38 @@ export async function POST(request: any) {
   const global_user_email: any | null | undefined =
     user?.emailAddresses[0].emailAddress;
 
-  if (user) {
-    try {
-      const body = await request.json();
-      const existingModel =
-        body.email === "existing"
-          ? await clientModel.findOne({ email: global_user_email })
-          : await clientModel.findOne({ email: body.email });
-      if (existingModel) {
-        console.log("Updating existing client...");
-        if (body.password !== "existing")
-          existingModel.password = body.password;
-        if (body.firstName !== "existing")
-          existingModel.firstName = body.firstName;
-        if (body.lastName !== "existing")
-          existingModel.lastName = body.lastName;
-        if (body.address !== "existing") existingModel.address = body.address;
-        await existingModel.save();
-        console.log("saving the data for an existing user.... ", body.id);
-      } else {
-        console.log("Creating new client...");
-        const new_client = new clientModel({
-          username: body.firstName,
-          email: body.email,
-          firstName: body.firstName,
-          lastName: body.lastName,
-          password: body.password,
-          address: body.address,
-          cart: [],
-          wishlist: [],
-        });
-        await new_client.save();
-      }
-      return new NextResponse("success");
-    } catch (error) {
-      console.error("Error in API route: ", error);
-      return new NextResponse("error");
+  try {
+    const body = await request.json();
+    const existingModel =
+      body.email === "existing"
+        ? await clientModel.findOne({ email: global_user_email })
+        : await clientModel.findOne({ email: body.email });
+    if (existingModel) {
+      console.log("Updating existing client...");
+      if (body.password !== "existing") existingModel.password = body.password;
+      if (body.firstName !== "existing")
+        existingModel.firstName = body.firstName;
+      if (body.lastName !== "existing") existingModel.lastName = body.lastName;
+      if (body.address !== "existing") existingModel.address = body.address;
+      await existingModel.save();
+      console.log("saving the data for an existing user.... ", body.id);
+    } else {
+      console.log("Creating new client...");
+      const new_client = new clientModel({
+        username: body.firstName,
+        email: body.email,
+        firstName: body.firstName,
+        lastName: body.lastName,
+        password: body.password,
+        address: body.address,
+        cart: [],
+        wishlist: [],
+      });
+      await new_client.save();
     }
-  } else {
-    console.log("No user is signed in.");
+    return new NextResponse("success");
+  } catch (error) {
+    console.error("Error in API route: ", error);
     return new NextResponse("error");
   }
 }
